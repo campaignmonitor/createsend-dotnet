d8990d816122bd7708003d499727da9e4b27a052
Brett Hoffman <bretth@campaignmonitor.com>
2015-05-21 11:29:52 +1000

    Added ReplyTo to message details response

--- createsend-dotnet/Transactional/MessageContent.cs
+++ createsend-dotnet/Transactional/MessageContent.cs
@@ -4,6 +4,7 @@ namespace createsend_dotnet.Transactional
     public class MessageContent
     {
         public EmailAddress From { get; set; }
+        public EmailAddress ReplyTo { get; set; }
         public string Subject { get; set; }
         public EmailAddress[] To { get; set; }
         public EmailAddress[] CC { get; set; }
