cfb2c60cb3ac1624fb96f8685bfcc651fbf23ed2
Cameron Newman <cameronn@campaignmonitor.com>
2017-04-21 11:41:27 +1000

    New Events Class

---
+++ createsend-dotnet/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace createsend_dotnet
+{
+    public class ContactID
+    {
+        public string Email { get; set; }
+    }
+
+    public class BasicEvent
+    {
+        public ContactID ContactID { get; set; }
+        public string EventName { get; set; }
+        public object Data { get; set; }
+    }
+
+    public class BasicEventResult
+    {
+        public string EventID { get; set; }
+    }
+}
