00ac023299b0b0d69007ef9509d424041d55d913
Jan Kolk <jan.kolk@spring-it.nl>
2018-01-29 11:26:22 +0100

    Working version with dotnet core, compiles and runs

--- createsend-dotnet/CreateSendOptions.cs
+++ createsend-dotnet/CreateSendOptions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+#if !NETSTANDARD2_0
 using System.Configuration;
+#endif
 
 namespace createsend_dotnet
 {
@@ -8,8 +10,14 @@ namespace createsend_dotnet
         static string base_uri;
         static string base_oauth_uri;
 
+#if NETSTANDARD2_0
+
+        public static ICreateSendOptions Options = null;
+#endif
         static CreateSendOptions()
         {
+
+#if !NETSTANDARD2_0 
             base_uri = string.IsNullOrEmpty(
                 ConfigurationManager.AppSettings["base_uri"]) ?
                 "https://api.createsend.com/api/v3.1" :
@@ -18,6 +26,11 @@ namespace createsend_dotnet
                 ConfigurationManager.AppSettings["base_oauth_uri"]) ?
                 "https://api.createsend.com/oauth" :
                 ConfigurationManager.AppSettings["base_oauth_uri"];
+#endif
+#if NETSTANDARD2_0
+            base_uri = Options != null ? Options.BaseUri : "https://api.createsend.com/api/v3.1";
+            base_oauth_uri = Options != null ? Options.BaseOAuthUri : "https://api.createsend.com/oauth";
+#endif
         }
 
         public static string BaseUri
