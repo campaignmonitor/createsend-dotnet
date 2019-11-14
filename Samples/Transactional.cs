6af1c57a2dd2a07c3dbaa6ee55fab9b442d2a871
Mark Staples <marks@campaignmonitor.com>
2019-11-14 21:24:50 +1100

    Add code sample for Transactional (#62)
    * Add code sample for Transactional Classic Email
    
    * Remove TX sample from .Net20
    
    * Minor spelling fix.

---
+++ Samples/Transactional.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using createsend_dotnet;
+using createsend_dotnet.Transactional;
+
+namespace Samples
+{
+    public class Transactional
+    {
+        private readonly IAgencyTransactional accountTransationalContext = Authenticate.ByAccountApiKey("your account api key");
+        private readonly ITransactional clientTransationalContext = Authenticate.ByClientApiKey("your client api key");
+
+        public void SendClassicEmailUsingAccountAPIKey()
+        {
+            try
+            {
+                var result = accountTransationalContext.ClassicEmail.Send("your client id", "no-reply@abcwidgets.com", "Tranasactional Classic Email",
+                    "<html><body>TEST EMAIL</body></html>", "TEST EMAIL", ConsentToTrack.Yes, to: "example@abcwidgets.com");
+
+                foreach (var recipientStatus in result.Response)
+                {
+                    Console.WriteLine($"{recipientStatus.Recipient.Email}:{recipientStatus.Status}");
+                }
+            }
+            catch (CreatesendException ex)
+            {
+                var error = (ErrorResult)ex.Data["ErrorResult"];
+                Console.WriteLine(error.Code);
+                Console.WriteLine(error.Message);
+            }
+            catch (Exception ex)
+            {
+                // Handle some other failure
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void SendClassicEmailUsingClientAPIKey()
+        {
+            try
+            {
+                var result = clientTransationalContext.ClassicEmail.Send("no-reply@abcwidgets.com", "Tranasactional Classic Email",
+                    "<html><body>TEST EMAIL</body></html>", "TEST EMAIL", ConsentToTrack.Yes, to: "example@abcwidgets.com");
+
+                foreach (var recipientStatus in result.Response)
+                {
+                    Console.WriteLine($"{recipientStatus.Recipient.Email}:{recipientStatus.Status}");
+                }
+            }
+            catch (CreatesendException ex)
+            {
+                var error = (ErrorResult)ex.Data["ErrorResult"];
+                Console.WriteLine(error.Code);
+                Console.WriteLine(error.Message);
+            }
+            catch (Exception ex)
+            {
+                // Handle some other failure
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
