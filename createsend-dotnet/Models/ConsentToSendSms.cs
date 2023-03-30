44e652071d4af1486cfde0f6b462d5d8b92411a4
FRESHVIEW\challowes <challowes@campaignmonitor.com>
2023-03-30 17:09:12 +1100

    SMS-598 - changes for earlier framewoks

---
+++ createsend-dotnet/Models/ConsentToSendSms.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace createsend_dotnet
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ConsentToSendSms
+    {
+        Unchanged = 0,
+        Yes = 1,
+        No = 2
+    }
+}
